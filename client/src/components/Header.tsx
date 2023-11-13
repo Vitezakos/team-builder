9861fe603764d02ed827aa578b579270df2066a5
Vitezakos <vitezakosh@gmail.com>
2023-11-13 13:20:49 +0100

    Created the search popup

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,23 @@
 import "./Header.css";
 
 function Header() {
+  function openSearch() {
+    document.querySelector(".searchContainer")?.classList.toggle("hidden");
+  }
   return (
     <div className="headerContainer">
-      <button>Search for player</button>
+      <button onClick={openSearch}>Search for player</button>
       <button>Team builder</button>
+      <div className="searchContainer hidden">
+        <div className="background">
+          <button className="serverBtn">Euw</button>
+          <input
+            className="searchBar"
+            type="text"
+            placeholder="Summoner name..."
+          />
+        </div>
+      </div>
     </div>
   );
 }
