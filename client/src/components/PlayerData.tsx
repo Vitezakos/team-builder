bcdceb5c57d7775d68c1debaa31ae41f317ea8f3
Vitezakos <vitezakosh@gmail.com>
2023-11-22 15:02:36 +0100

    Fixed routing according to riot's new system

--- client/src/components/PlayerData.tsx
+++ client/src/components/PlayerData.tsx
@@ -1,48 +1,61 @@
 import "./PlayerData.css";
 //import poroIcon from "../icons/3009.png";
 import Ksante from "../icons/ksante.png";
-//import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function PlayerData({ profileName }: any) {
   let riotName = profileName.split("#")[0];
   let riotTagLine = profileName.split("#")[1];
   console.log(riotName, riotTagLine);
-  //const apiKey = "";
-  // const [iconId, setIconId] = useState(0);
-  //let accountLink = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${profileName}?api_key=RGAPI-${apiKey}`;
-  let riotAccountLink = `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/NightOwl/8512?api_key=RGAPI-`;
-  let summonerIcon = `https://ddragon.leagueoflegends.com/cdn/10.18.1/img/profileicon/0.png`;
-  //const [summonerName, setSummonerName] = useState("");
-  //const [summonerLevel, setSummonerLevel] = useState(0);
+  const apiKey = "4beb3645-a899-48b7-8fbd-c12c81b3ff2b";
+  const [iconId, setIconId] = useState(0);
+  let riotAccountLink = `/riot-api/riot/account/v1/accounts/by-riot-id/${riotName}/${riotTagLine}?api_key=RGAPI-${apiKey}`;
+  let summonerIcon = `https://ddragon.leagueoflegends.com/cdn/10.18.1/img/profileicon/${iconId}.png`;
+  const [leagueName, setLeagueName] = useState("");
+  const [summonerLevel, setSummonerLevel] = useState(0);
+  let [puuid, setPuuid] = useState("");
   async function data() {
-    const main = await fetch(riotAccountLink, {
-      method: "GET",
-      headers: {
-        "X-Requested-With": "XMLHttpRequest", // Add your custom headers here
-      },
-    });
-    // .then((main) => {
-    //   if (!main.ok) {
-    //     throw new Error("Error");
-    //   }
-    //   return main.json();
-    // });
-    // const info = await main;
-    const info = await main.json();
-    console.log(info);
-    //setIconId(info.profileIconId);
-    //setSummonerName(info.name);
-    //setSummonerLevel(info.summonerLevel);
+    try {
+      const response = await fetch(riotAccountLink);
+      const data = await response.json();
+
+      console.log("Data:", data);
+      setPuuid(data.puuid);
+      setLeagueName(data.gameName);
+      // Do something with the data
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      // Handle errors appropriately
+    }
   }
   data();
-  console.log(/*iconId*/);
+
+  async function summonerData(ar: string) {
+    try {
+      const response = await fetch(ar);
+      const data = await response.json();
+
+      console.log("summonerData:", data);
+      setIconId(data.profileIconId);
+      setSummonerLevel(data.summonerLevel);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+  useEffect(() => {
+    if (!(puuid == "")) {
+      let puuidLink = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=RGAPI-${apiKey}`;
+      summonerData(puuidLink);
+    }
+  });
+
   console.log("playerdata was rendered");
   return (
     <div className="player-container">
       <div className="summoner">
         <img src={summonerIcon} alt="" />
-        <h1 className="summoner-name">{/*summonerName*/}</h1>
-        <h1 className="summoner-level">Lv. {/*summonerLevel*/}</h1>
+        <h1 className="summoner-name">{leagueName + "#" + riotTagLine}</h1>
+        <h1 className="summoner-level">Lv. {summonerLevel}</h1>
       </div>
       <div className="match">
         <img src={Ksante} />
