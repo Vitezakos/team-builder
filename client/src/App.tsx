1b5e219e4e0c65dba13dd7d8cf219def3234ef9a
Vitezakos <vitezakosh@gmail.com>
2024-02-20 14:43:05 +0100

    swapped to hashrouter, changed homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from "./components/Header";
 import { TeamBuilderContainer } from "./components/TeamBuilderContainer";
 import "semantic-ui-css/semantic.min.css";
 import { PlayerData } from "./components/PlayerData";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { useState } from "react";
 import { playerContext } from "./components/utilities/useContext";
 
@@ -21,7 +21,7 @@ function App() {
     <>
       <playerContext.Provider value={value}>
         <div className="app-container">
-          <BrowserRouter basename="/">
+          <HashRouter basename="/">
             <Routes>
               <Route
                 index
@@ -43,7 +43,7 @@ function App() {
                 }
               />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </div>
       </playerContext.Provider>
     </>
