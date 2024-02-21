2cef30f9ee41da3d53e6757c665cc11f535d5796
Vitezakos <vitezakosh@gmail.com>
2024-02-21 15:47:54 +0100

    fetch changed

--- client/src/components/useProfileState.ts
+++ client/src/components/useProfileState.ts
@@ -24,8 +24,9 @@ const useProfileState = () => {
 
   useEffect(() => {
     const [riotName, riotTagLine] = inputName.split("#");
-    let riotAccountLink = `/riot-api/riot/account/v1/accounts/by-riot-id/${riotName}/${riotTagLine}?api_key=${apiKey}`;
-    handleCurrentGames(riotAccountLink);
+    //let riotAccountLink = `/riot-api/riot/account/v1/accounts/by-riot-id/${riotName}/${riotTagLine}?api_key=${apiKey}`;
+    let riotAccountLink2 = `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${riotName}/${riotTagLine}?api_key=${apiKey}`;
+    handleCurrentGames(riotAccountLink2);
   }, [inputName]);
 
   const handleCurrentGames = async (riotApiCall: string) => {
